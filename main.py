4621eb91fa453e23134b23d3765afcac6916d2e9
Francisco Oliveira <fr.pdoliv@gmail.com>
2022-04-14 23:30:24 +0100

    Started to work on menus

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
-from state import RobotState
-from maze import Direction, Maze, Position
-import algorithms
+import pygame
+import pygame_menu
+
+from typing import Optional
+
+WINDOW_SIZE = (900, 600)
+
+main_menu: Optional['pygame_menu.Menu'] = None
+surface: Optional['pygame'] = None
+
+def choose_map():
+    pass
+
+def main():
+    global main_menu
+    global surface
+
+    pygame.init()
+
+    surface = pygame.display.set_mode(WINDOW_SIZE)
+    main_menu = pygame_menu.Menu(
+        height=WINDOW_SIZE[1],
+        width=WINDOW_SIZE[0],
+        title="Robot Mazes",
+        theme=pygame_menu.themes.THEME_DARK
+    )
+
+    main_menu.add.button("Choose Map", choose_map)
+    main_menu.add.button("Quit", pygame_menu.events.EXIT)
+
+    while True:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                exit()
+        if main_menu.is_enabled():
+            main_menu.mainloop(surface)
+
+        pygame.display.flip()
 
 if __name__ == "__main__":
-    maze = Maze(
-        4,
-        Position(3, 0),
-        Position(0, 3),
-        [
-            (Position(2, 0), Position(2, 1)),
-            (Position(1, 1), Position(1, 2)),
-            (Position(0, 1), Position(0, 2)),
-            (Position(0, 3), Position(1, 3)),
-            (Position(1, 3), Position(2, 3))
-        ]
-    )
-    print(algorithms.a_star_search(maze, 4, lambda state: 1))
-
+    main()
